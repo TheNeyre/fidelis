0a62c8591b9c62f90cb9da94331f16667a5db7e0
TheNeyre <163881074+TheNeyre@users.noreply.github.com>
2026-09-06 00:17:14 +0300

    fix header

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import MainPage from "./pages/main/page";
 import NotFoundPage from "./pages/notfound/page";
 import { useRoutes } from "react-router-dom";
 import "./normalize.css";
-import "./globals.css";
+import "./globals.scss";
 export default function App () {
   return useRoutes([
     { path: "/", element: ( <MainPage/>) },
--- src/pages/common/header/component.jsx
+++ src/pages/common/header/component.jsx
@@ -3,16 +3,15 @@ export default function Header () {
 
   return ( <header id="header" className={styles.headerContainer}>
 
-    <div className={styles.logoContainer}>
-      <img src="/icons/header.svg" alt="fidelis-header-logo" className={styles.logo}/>
-    </div>
+    <img src="/icons/header.svg" alt="fidelis-header-logo" className={styles.logo}/>
 
     <ul className={styles.header}>
-      <li className={styles.headerElement}> {"главная"} </li>
+      <li className={`${styles.headerElement} ${styles.firstHeaderElement}`}> {"главная"} </li>
       <li className={styles.headerElement}> {"о нас"} </li>
       <li className={styles.headerElement}> {"ассортимент"} </li>
       <li className={styles.headerElement}> {"контакты"} </li>
-      <li className={styles.headerElement}> {"связаться"} <img src="/icons/arrow-right" alt="contact-us-icon" className={styles.contactUsIcon}/> </li>
+      <li className={`${styles.headerElement} ${styles.lastHeaderElement}`}> <p className={styles.lastHeaderElementContent}>{"связаться"}</p>
+      <img src="/icons/arrow-right.svg" alt="contact-us-icon" className={styles.contactUsIcon}/> </li>
     </ul>
 
   </header> )
