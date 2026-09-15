3976d33d756bb40702c65ee27961e6a45cbd5919
TheNeyre <163881074+TheNeyre@users.noreply.github.com>
2026-09-15 21:06:45 +0300

    add spawn animation wrapper (effects)

--- src/pages/common/effects/component.tsx
+++ src/pages/common/effects/component.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { Children, useEffect, useRef, useState } from "react";
 import styles from "./effects.module.scss";
 
 interface ProgressiveLayerBlurProps {
@@ -26,5 +26,23 @@ export const ProgressiveLayerBlur: React.FC<ProgressiveLayerBlurProps>
 
   return ( <div className={styles.progressiveLayerBlur} style={getCssVariables()}>
     {Array(5).fill(null).map((_, index) => ( <div key={index} className={styles.blurLayer}/> ))}
+  </div> )
+}
+
+export const SpawnAnimationWrapper: React.FC<{children: React.ReactElement}> = ({children}) => {
+  const spawnerRef = useRef<HTMLDivElement>(null);
+  useEffect(()=>{
+    const spawner = spawnerRef.current;
+    if (!spawnerRef || !spawner) return;
+    const scrollCheck = () => {
+      const bottomTrigger = window.innerHeight*0.8;
+      const currentTop = spawner.getBoundingClientRect().top;
+      if (currentTop < bottomTrigger) spawner.classList.add(styles.spawn);
+    }
+    scrollCheck(); window.addEventListener("scroll", scrollCheck);
+    return () => window.removeEventListener("scroll", scrollCheck);
+  },[]);
+  return ( <div className={styles.spawnAnimationWrapper} ref={spawnerRef}>
+    {children}
   </div> )
 }
