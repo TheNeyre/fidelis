9baa0a7264c98039b160b62d8eb420d2fabab5b8
TheNeyre <163881074+TheNeyre@users.noreply.github.com>
2026-09-19 16:13:02 +0300

    add form

--- src/pages/main/components/form/component.tsx
+++ src/pages/main/components/form/component.tsx
@@ -1,63 +1,98 @@
 import styles from "./component.module.scss"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { SpawnAnimationWrapper } from "../../../common/effects/component";
 export default function BitrixForm () {
 
   const [ formStatus, setFormStatus ] = useState<string>("default");
-  const handleFormSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+  const handleFormSubmit = async (event: React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault();
     setFormStatus("loading");
     const formData = new FormData(event.target);
     const name = formData.get("name")?.toString();
     const email = formData.get("email")?.toString();
     const phone = formData.get("phone")?.toString();
-    if (!email || !phone || !name) { setFormStatus("not-full-data"); return }
-    if (email && !email.includes(".") && !email.includes("@") && !(email.length >= 5)) { setFormStatus("incorrect-data"); return}
-    if (phone && !(phone.length>10)) { setFormStatus("incorrect-data"); return }
+    if (!email || !phone || !name) { setFormStatus("not-full-data-error"); return }
+    if (email && !email.includes(".") && !email.includes("@") && !(email.length >= 5)) { setFormStatus("incorrect-data-error"); return}
+    if (phone && !(phone.length>10)) { setFormStatus("incorrect-data-error"); return }
+    
+    const response = await fetch("/api/send_form/", {
+      method: "POST",
+      body: JSON.stringify({name, email, phone}),
+    });
+    if (!response.ok) {setFormStatus("req-error"); return}
+
   }
 
   const formStatusTextTemplates: { [key: string]: string } = {
     "default": "Введите свои данные для связи",
-    "incorrect-data": "Вы некорректно вели данные! Проверьте ещё раз!",
-    "not-full-data": "Все поля обязательны для заполнения!",
+    "incorrect-data-error": "Вы некорректно вели данные! Проверьте ещё раз!",
+    "not-full-data-error": "Все поля обязательны для заполнения!",
     "button:loading": "Загрузка...",
     "button:success": "Успешно!",
     "button:default": "Отправить",
   }
 
-  return ( <div className={styles.bitrixFormContainer}>
+  useEffect(()=>{
+    const formInputs = document.querySelectorAll<HTMLInputElement>(`.${styles.formInputContainer}`);
+    const onInputChange = (event: Event) => {
+      const parent = event.currentTarget as HTMLDivElement;
+      const target = event.target as HTMLInputElement;
+      if (!target || !parent) return;
+      if (target.value) parent.classList.add(styles.focused);
+      if (document.activeElement === target) return;
+      else parent.classList.remove(styles.focused);
+    }
+    const oninputFocusIn = (event: Event) => {
+      const parent = event.currentTarget as HTMLDivElement;
+      const target = event.target as HTMLInputElement;
+      if (target.value) return;
+      parent.classList.add(styles.focused);
+    }
+    const oninputFocusOut = (event: Event) => {
+      const parent = event.currentTarget as HTMLDivElement;
+      const target = event.target as HTMLInputElement;
+      if (target.value) return;
+      parent.classList.remove(styles.focused);
+    }
+    formInputs.forEach((formInput) => {
+      formInput.addEventListener("input", onInputChange);
+      formInput.addEventListener("focusin", oninputFocusIn);
+      formInput.addEventListener("focusout", oninputFocusOut);
+    });
+    return () => formInputs.forEach((formInput) => {
+      formInput.removeEventListener("input", onInputChange);
+      formInput.removeEventListener("focusin", oninputFocusIn);
+      formInput.removeEventListener("focusout", oninputFocusOut);
+    });
+  }, [])
 
-    <div className={styles.bitrixFormContainerContent}>
-    <div className={styles.bitrixFormTitle}>
-      Интересно? <br /> Отправьте нам свою заявку!
+  return ( <SpawnAnimationWrapper>
+  <div className={styles.bitrixFormContainer} id="form">
+      <div className={styles.bitrixFormContainerContent}>
+      <div className={styles.bitrixFormTitle}>
+        Интересно? <br /> Отправьте нам свою заявку!
+      </div>
+      <form className={styles.bitrixForm} onSubmit={(e)=>handleFormSubmit(e)}>
+        <div className={`${styles.formStatus} ${formStatus.includes("error")?styles.formStatusError:""}`}>
+          {formStatusTextTemplates[formStatus]}
+        </div>
+        <div className={styles.formInputContainer}>
+          <input type="text" name="name" className={styles.formInput}/>
+          <div className={styles.additionText}>{"Как в вам обращаться?"}</div>
+        </div>
+        <div className={styles.formInputContainer}>
+          <input type="text" name="email" className={styles.formInput}/>
+          <div className={styles.additionText}>{"Ваша почта"}</div>
+        </div>
+        <div className={styles.formInputContainer}>
+          <input type="text" name="phone" className={styles.formInput}/>
+          <div className={styles.additionText}>{"Номер телефона"}</div>
+        </div>
+        <button type="submit" className={`${styles.submitButton} ${formStatus=="loading"?styles.submitButtonLoading:""} ${formStatus=="success"?styles.submitButtonSuccess:""}`}>{["loading", "success", "default"]
+        .includes(formStatus)?formStatusTextTemplates[`button:${formStatus}`]:formStatusTextTemplates["button:default"]}</button>
+      </form>
+      </div> 
+
     </div>
-
-    <form className={styles.bitrixForm} onSubmit={(e)=>handleFormSubmit(e)}>
-
-      <div className={`${styles.formStatus} ${formStatus.includes("error")?styles.formStatusError:""}`}>
-        {formStatusTextTemplates[formStatus]}
-      </div>
-
-      <div className={styles.formInputContainer}>
-        <input type="text" name="name" className={styles.formInput}/>
-        <div className={styles.additionText}>{"Как в вам обращаться?"}</div>
-      </div>
-
-      <div className={styles.formInputContainer}>
-        <input type="text" name="email" className={styles.formInput}/>
-        <div className={styles.additionText}>{"Ваша почта"}</div>
-      </div>
-
-      <div className={styles.formInputContainer}>
-        <input type="text" name="phone" className={styles.formInput}/>
-        <div className={styles.additionText}>{"Номер телефона"}</div>
-      </div>
-
-      <button className={styles.submitButton }>{formStatusTextTemplates[`button:${formStatus}`]}</button>
-    </form>
-    </div>
-
-
-
-
-  </div> )
+  </SpawnAnimationWrapper> )
 }
